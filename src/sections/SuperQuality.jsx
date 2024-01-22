153733996b93644ee6509f59870cf91dd45ba291
Idris Noor <idreesnoor150@gmail.com>
2024-01-22 16:41:27 +0300

    Nike Landing Page With Tailwind css

---
+++ src/sections/SuperQuality.jsx
@@ -0,0 +1,43 @@
+import { shoe8 } from "../assets/images";
+import { Button } from "../components";
+
+const SuperQuality = () => {
+  return (
+    <section
+      id="about-us"
+      className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container"
+    >
+      <div className="flex flex-1 flex-col">
+        <h2 className="font-palanquin capitalize text-4xl lg:max-w-lg font-bold">
+          We Provide You
+          <span className="text-coral-red"> Super </span>
+          <span className="text-coral-red">Quality </span> Shoes
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          Elevate every step with our crafted footwear, blending premium
+          comfort, style, and innovation for an unmatched experience in quality
+          and elegance.
+        </p>
+        <p className="mt-6 lg:max-w-lg info-text">
+          Detail and excellence define us, guaranteeing satisfaction through our
+          unwavering dedication.{" "}
+        </p>
+        <div className="mt-11">
+          <Button label="View details" />
+        </div>
+      </div>
+
+      <div className="flex-1 flex justify-center items-center">
+        <img
+          src={shoe8}
+          alt="product detail"
+          width={570}
+          height={522}
+          className="object-contain"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default SuperQuality;
